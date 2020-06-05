37cbcd7426a2ec48404c19027bca6d41b99f0399
byenice <66467806+byenice@users.noreply.github.com>
2020-06-05 12:27:16 +0300

    Arrays

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] students = new string[3] { "Engin", "Berk", "Salih" };
+
+            string[] students2 = {"Engin", "Berk", "Salih"};
+
+            foreach (var student in students2)
+            {
+                Console.WriteLine(student);
+            }
+            Console.WriteLine("---------");
+
+            string[,] regions=new string[5,3]
+            {
+                {"İstanbul","İzmit","Balıkesir"},
+                {"Ankara","Konya","Kırıkkale"},
+                {"Antalya","Adana","Mersin"},
+                {"Rize","Trabzon","Samsun"},
+                {"İzmir","Muğla","Manisa"},
+            };
+
+            for (int i = 0; i <= regions.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= regions.GetUpperBound(1); j++)
+                {
+                    Console.WriteLine(regions[i,j]);
+                }
+                Console.WriteLine("*********");
+            }
+
+            Console.ReadLine();
+
+            //string student1 = "Engin";
+            //string student2 = "Berk";
+            //string student3 = "Salih";
+
+
+        }
+    }
+}
